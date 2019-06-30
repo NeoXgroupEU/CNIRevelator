6c2573905176520220399d6e1867273d519150bc
neox95 <neox@os-k.eu>
2019-06-30 17:54:41 +0200

    v2.2.5

--- CNI_GLOBALVAR.py
+++ CNI_GLOBALVAR.py
@@ -10,41 +10,38 @@
 
 ********************************************************************************
 """
-CST_REV = "8"
-CST_VERTITLE = "2.2"
-CST_TAB_VER = ["2","2","1"]
-CST_VER = "{0}.{1}.{2}".format(CST_TAB_VER[0], CST_TAB_VER[1], CST_TAB_VER[2])
-CST_TYPE = "Final Release"
-CST_NAME = "CNIRevelator"
-CST_TITLE = CST_NAME + " " +  CST_VER + " - GNU/GPL Licensing 2018"
-CST_MARK = CST_NAME + " " + CST_TYPE + " " + CST_VER + " - by NeoX, GNU/GPL Licensing 2018"
-CST_SUM_VER = int(CST_TAB_VER[0])*100 + int(CST_TAB_VER[1])*10 + int(CST_TAB_VER[2])
-CST_LINK = <lien vers serveur>
-CST_COLOR = "#003380"
+
+class changelog:
+
+    def __init__(self):
+        self.isOn = False
+        self.text = "Mise-à-jour de sécurité avec corrections suivantes :\n- ajout de la signature numérique de l'exécutable\n- ajout d'une clé de cryptage plus performante pour le stockage des identifiants\n- passage à la méthode de cryptage AES256\n\nAjout/correction des fonctionnalités suivantes :\n- somme de contrôle des téléchargements pour une meilleure fiabilité\n- amélioration de présentation du log en cas d'erreur\n- correction d'un bug affectant l'analyse des MRZ après la correction manuelle\n\nEt un petit bonjour à tout le monde! ;)"
 
 
-import base64
-import hashlib
+CST_REV = '0'
+CST_VERTITLE = '2.2'
+CST_TAB_VER = ['2', '2', '5']
+CST_VER = '{0}.{1}.{2}'.format(CST_TAB_VER[0], CST_TAB_VER[1], CST_TAB_VER[2])
+CST_TYPE = 'Final Release'
+CST_NAME = 'CNIRevelator'
+CST_TITLE = CST_NAME + ' ' + CST_VER + ' - GNU/GPL Licensing 2018'
+CST_MARK = CST_NAME + ' ' + CST_TYPE + ' ' + CST_VER + ' - by NeoX, GNU/GPL Licensing 2018'
+CST_SUM_VER = int(CST_TAB_VER[0]) * 100 + int(CST_TAB_VER[1]) * 10 + int(CST_TAB_VER[2])
+CST_LINK = 'http://neoxgroup.eu/ftpaccess/Applicatifs/CNIRevelator/'
+CST_COLOR = '#003380'
+CST_TesserHash = '5b58db27f7bc08c58a2cb33d01533b034b067cf8'
+
+
+import base64, hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter import filedialog
-from tkinter import ttk as ttk
-import os
-import time 
-import threading
-import sys
-import urllib.request as urllib2
-import urllib.error as URLExcept
-import random
-from pypac import PACSession
-from requests.auth import HTTPProxyAuth
-import subprocess
+from tkinter import ttk
+import os, time, threading, sys, urllib.request as urllib2, urllib.error as URLExcept, random
 from datetime import datetime
-from PIL import Image, ImageFont, ImageDraw, ImageTk, ImageEnhance, ImageFilter
-import math
-import warnings
-import string
 
-CST_FOLDER = os.getenv('APPDATA') + "/CNIRevelator/"
+CST_FOLDER = os.getenv('APPDATA') + '/CNIRevelator/'
+CST_CRYPTOKEY = '82Xh!efX3#@P~2eG'
+CST_CHANGELOG = changelog()
--- CNI_Revelator.py
+++ CNI_Revelator.py
@@ -10,122 +10,97 @@
 
 ********************************************************************************
 """
-
-###IMPORTS GLOBAUX
-from CNI_GLOBALVAR import * 
-
-
-##LOGGING
-
+from CNI_GLOBALVAR import *
 try:
-    os.remove("error.log")
-    os.remove("conf.ig")
+    os.remove('error.log')
+    os.remove('conf.ig')
 except:
+    print("pass log deletion")
     pass
-import logging
 
-CST_NIVEAU_LOG = logging.ERROR
-
-from logging.handlers import RotatingFileHandler
-
-# création de l'objet logger qui va nous servir à écrire dans les logs
-logger = logging.getLogger()
-# on met le niveau du logger à DEBUG, comme ça il écrit tout
-logger.setLevel(CST_NIVEAU_LOG)
-
-# création d'un formateur qui va ajouter le temps, le niveau
-# de chaque message quand on écrira un message dans le log
-formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-# création d'un handler qui va rediriger une écriture du log vers
-# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
-file_handler = RotatingFileHandler('error.log', 'a', 1000000, 1)
-# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
-# créé précédement et on ajoute ce handler au logger
-file_handler.setLevel(CST_NIVEAU_LOG)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-##History
-
-
-
-### IMPORTS LOCAUX       
-from CNI_classes import *
-from CNI_Update import *  
-
-### FONCTION PRINCIPALE
-
-
-def main(logger):
-    
-    logger.info("main() : " +"**** Creating App_main() ****")
-    main_w = App_main(logger)
-    
-    main_w.montext("* " + CST_NAME + " " + CST_VER + " " + CST_TYPE + " Revision " + CST_REV + " *\n")
-    
-    import CNI_pytesseract as pytesseract
-    
+if not os.path.exists(CST_FOLDER):
     try:
-        
-        os.environ["PATH"] = CST_FOLDER + "Tesseract-OCR4\\" 
-        os.environ["TESSDATA_PREFIX"] = CST_FOLDER + "Tesseract-OCR4\\tessdata"
-        tesser_version = pytesseract.get_tesseract_version()
-        
+        os.makedirs(CST_FOLDER)
+    except IOError:
+        print("pass IO ERROR")
+        pass
+
+    import logging
+    from logging import FileHandler
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+    error_handler = FileHandler((CST_FOLDER + '\\error.log'), mode='w', encoding='utf-8', delay=True)
+    info_handler = FileHandler((CST_FOLDER + '\\cnirevelator.log'), mode='w', encoding='utf-8')
+    error_handler.setLevel(logging.ERROR)
+    logger.addHandler(error_handler)
+    info_handler.setLevel(logging.DEBUG)
+    info_handler.setFormatter(formatter)
+    logger.addHandler(info_handler)
+    from CNI_classes import *
+    from CNI_Update import *
+
+    def main(logger):
+        logger.error('')
+        logger.info('main() : **** Creating App_main() ****')
+        main_w = App_main(logger)
+        main_w.montext('* ' + CST_NAME + ' ' + CST_VER + ' ' + CST_TYPE + ' Revision ' + CST_REV + ' *\n')
+        import CNI_pytesseract as pytesseract
+        try:
+            os.environ['PATH'] = CST_FOLDER + 'Tesseract-OCR4\\'
+            os.environ['TESSDATA_PREFIX'] = CST_FOLDER + 'Tesseract-OCR4\\tessdata'
+            tesser_version = pytesseract.get_tesseract_version()
+        except Exception as e:
+            logger.error('main() : **** ERROR WITH TESSERACT MODULE ' + str(e) + ' ****')
+        else:
+            text = 'Tesseract version ' + str(tesser_version) + ' Licensed Apache 2004 successfully initiated\n'
+            main_w.montext(text)
+        main_w.montext('\n\nEntrez la première ligne de MRZ svp \n')
+        if CST_CHANGELOG.isOn:
+            showinfo('Changelog : résumé de mise à jour', ('Version du logiciel : ' + CST_VER + ' ' + CST_TYPE + ' Revision ' + CST_REV + '\n\n' + CST_CHANGELOG.text), parent=main_w)
+        logger.info('main() : **** Launching App_main() ****')
+        main_w.mainloop()
+        logger.info('main() : **** Ending App_main() ****')
+
+
+    logger.info('launcher : ' + CST_NAME + ' ' + CST_VER)
+    logger.info('launcher : *****Hello World*****')
+    logger.info('launcher : *****Launching SoftUpdate()*****')
+    try:
+        Answer = SoftUpdate(logger)
     except Exception as e:
-        logger.error("main() : " +"**** ERROR WITH TESSERACT MODULE " + str(e) + " ****")
-    else:
-        text = "Tesseract version " + str(tesser_version) +" Licensed Apache 2004 successfully initiated\n"
-        main_w.montext(text)
-        
-    
-    main_w.montext("\n\nEntrez la première ligne de MRZ svp \n")
-    
-    
-    
-    logger.info("main() : " +"**** Launching App_main() ****")
-    main_w.mainloop()
-    logger.info("main() : " +"**** Ending App_main() ****")
-    
-
-   
-##Launcher
-
-logger.info("launcher : " + CST_NAME +" "+ CST_VER)
-logger.info("launcher : " +"*****Hello World*****")
-
-logger.info("launcher : " +"*****Launching SoftUpdate()*****")
-try:
-    Answer = SoftUpdate(logger)
-except Exception as e:
-    logger.info("launcher : " +"*****FATAL ERROR*****" + str(e))
-    os.abort()
-logger.info("launcher : " +"*****Ending SoftUpdate()*****")
-
-
-try:
-    if Answer == True:
-        logger.info("launcher : " +"*****Launching main()*****")
-        State = main(logger)
-except Exception as e:
-    logger.info("launcher : " +"*****FATAL ERROR*****" + str(e))
-    os.abort()
-    
-logger.info("launcher : " +"*****Ending main()*****")
-logger.info("launcher : " +"*****Goodbye!*****")
-
-handlers = logger.handlers[:]
-for handler in handlers:
-    handler.close()
-    logger.removeHandler(handler)
-    
-    
-if os.path.getsize("error.log") == 0:
-    try:
-        os.remove("error.log")
-    except:
-        raise(OSError)
+        logger.info('launcher : *****FATAL ERROR*****' + str(e))
         os.abort()
 
-sys.exit(0)
+    logger.info('launcher : *****Ending SoftUpdate()*****')
+    try:
+        if Answer == True:
+            logger.info('launcher : *****Launching main()*****')
+            State = main(logger)
+    except Exception as e:
+        logger.info('launcher : *****FATAL ERROR*****' + str(e))
+        os.abort()
 
-    
+    logger.info('launcher : *****Ending main()*****')
+    logger.info('launcher : *****Goodbye!*****')
+    handlers = logger.handlers[:]
+    for handler in handlers:
+        handler.close()
+        logger.removeHandler(handler)
+
+    try:
+        with open(CST_FOLDER + '\\error.log') as (echo):
+            try:
+                os.remove('error.log')
+            except OSError:
+                pass
+
+            from shutil import copyfile
+            temptwo = str(echo.read())
+            if len(temptwo) != 1:
+                copyfile(CST_FOLDER + '\\cnirevelator.log', 'error.log')
+    except IOError:
+        pass
+
+    print("exit")
+    #sys.exit(0)
