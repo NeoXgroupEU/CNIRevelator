cac10bacc6a038fc8259d310d24098fb25319fa2
neox95 <neox@os-k.eu>
2019-07-01 23:33:05 +0200

    Begin reorganization

---
+++ make.bat
@@ -0,0 +1,9 @@
+@echo off
+title Compilation de CNIRevelator
+
+call pyinstaller -w -D --exclude-module PyQt5 --bootloader-ignore-signals --add-data "C:\users\adrie\Anaconda3\Lib\site-packages\tld\res\effective_tld_names.dat.txt";"tld\res" --add-data "id-card.ico";"id-card.ico" -i "id-card.ico" -n CNIRevelator src\analyzer\CNI_Revelator.py 
+
+copy LICENSE dist\CNIRevelator\
+
+pause
+
---
+++ src/launcher/globs.py
@@ -0,0 +1,29 @@
+"""
+********************************************************************************
+*                             CNIRevelator                                     *
+*                                                                              *
+*  Desc:       Application launcher global variables                           *
+*                                                                              *
+*  Copyright © 2018-2019 Adrien Bourmault (neox95)                             *
+*                                                                              *
+*  This file is part of CNIRevelator.                                          *
+*                                                                              *
+*  CNIRevelator is free software: you can redistribute it and/or modify        *
+*  it under the terms of the GNU General Public License as published by        *
+*  the Free Software Foundation, either version 3 of the License, or           *
+*  any later version.                                                          *
+*                                                                              *
+*  CNIRevelator is distributed in the hope that it will be useful,             *
+*  but WITHOUT ANY WARRANTY*without even the implied warranty of               *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+*  GNU General Public License for more details.                                *
+*                                                                              *
+*  You should have received a copy of the GNU General Public License           *
+* along with CNIRevelator. If not, see <https:*www.gnu.org/licenses/>.         *
+********************************************************************************
+"""
+import os
+
+CNIRTesserHash = '5b58db27f7bc08c58a2cb33d01533b034b067cf8'
+#CNIRFolder = os.getenv('APPDATA') + '/CNIRevelator/'
+CNIRFolder = '.'
