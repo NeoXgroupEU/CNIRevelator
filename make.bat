ac6b052c06839e3e02a9aa602c006a0ee3a0abd4
Adrien Bourmault <neox@os-k.eu>
2019-08-07 15:30:45 +0200

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,10 +1,21 @@
 @echo off
+
 title Compilation de CNIRevelator
 
-call pyinstaller -w -D --exclude-module PyQt5 --bootloader-ignore-signals --add-data "C:\users\adrie\Anaconda3\Lib\site-packages\tld\res\effective_tld_names.dat.txt";"tld\res" --add-data "id-card.ico";"id-card.ico" -i "id-card.ico" -n CNIRevelator src\CNIRevelator.py 
+
+
+call pyinstaller -w -D --exclude-module PyQt5 --bootloader-ignore-signals --add-data "C:\Users\pf04950\AppData\Local\Continuum\anaconda3\Lib\site-packages\tld\res\effective_tld_names.dat.txt";"tld\res" --add-data "id-card.ico";"id-card.ico" -i "id-card.ico" --add-data "background.png";"background.png" -i "background.png" -n CNIRevelator src\CNIRevelator.py 
+
+
 
 copy LICENSE dist\CNIRevelator\
-copy id-card.ico dist\CNIRevelator\
+
+copy src\id-card.ico dist\CNIRevelator\
+
+copy src\background.png dist\CNIRevelator\
+
+
+D:\Public\CNIRevelator-master\CNIRevelator-master\signtool_8.1\signtool\signtool.exe  sign /n "CNIRevelator by Adrien Bourmault (neox95)" dist\CNIRevelator\CNIRevelator.exe
 
 pause
 
